35bc6ee7fe9db400fc491ae28bb1171c72a107d2
Mahmud İçme <62525785+mamooo66@users.noreply.github.com>
2021-05-14 16:13:48 +0300

    Update InterstitialADS.cs

--- InterstitialADS.cs
+++ InterstitialADS.cs
@@ -1,72 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using GoogleMobileAds.Api;
 using System;
 
-public class InterstitialADS : MonoBehaviour
-{
-    private InterstitialAd interstitial;
-
-    private void RequestInterstitial()
-    {
-        #if UNITY_ANDROID
-            string adUnitId = "ca-app-pub-9511884521777731/9405280134";
-        #elif UNITY_IPHONE
-            string adUnitId = "ca-app-pub-9511884521777731/9405280134";
-        #else
-            string adUnitId = "ca-app-pub-9511884521777731/9405280134";
-        #endif
-
-
-        this.interstitial = new InterstitialAd(adUnitId);
-
-        // Called when an ad request has successfully loaded.
-        this.interstitial.OnAdLoaded += HandleOnAdLoaded;
-        // Called when an ad request failed to load.
-        this.interstitial.OnAdFailedToLoad += HandleOnAdFailedToLoad;
-        // Called when an ad is shown.
-        this.interstitial.OnAdOpening += HandleOnAdOpened;
-        // Called when the ad is closed.
-        this.interstitial.OnAdClosed += HandleOnAdClosed;
-        // Called when the ad click caused the user to leave the application.
-        this.interstitial.OnAdLeavingApplication += HandleOnAdLeavingApplication;
-
-        AdRequest request = new AdRequest.Builder().Build();
-
-        this.interstitial.LoadAd(request);
+public class InterstitialADS : MonoBehaviour {
+    private InterstitialAd InterAd;
+    public string InterID = "";
+    private void Start() {
+        MobileAds.Initialize(giveAD => { });
+        InterStatil();
     }
 
-    private void GameOver()
-    {
-        if (this.interstitial.IsLoaded()) {
-          this.interstitial.Show();
-        }
-    }
+    public void InterStatil(){
+        InterAd = new InterstitialAd(InterID);
+        AdRequest newAD = new AdRequest.Builder().Build();
 
-    public void HandleOnAdLoaded(object sender, EventArgs args)
-    {
-        MonoBehaviour.print("HandleAdLoaded event received");
-    }
-
-    public void HandleOnAdFailedToLoad(object sender, AdFailedToLoadEventArgs args)
-    {
-        MonoBehaviour.print("HandleFailedToReceiveAd event received with message: "
-                            + args.Message);
-    }
-
-    public void HandleOnAdOpened(object sender, EventArgs args)
-    {
-        MonoBehaviour.print("HandleAdOpened event received");
-    }
-
-    public void HandleOnAdClosed(object sender, EventArgs args)
-    {
-        MonoBehaviour.print("HandleAdClosed event received");
-    }
-
-    public void HandleOnAdLeavingApplication(object sender, EventArgs args)
-    {
-        MonoBehaviour.print("HandleAdLeavingApplication event received");
+        InterAd.LoadAd(newAD);
+        InterAd.Show();
     }
 }
